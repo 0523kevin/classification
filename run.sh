b2af1c6c402bc2d08fd3a299dadd11421f569875
IsSong <0523kevin@naver.com>
2023-04-19 23:44:58 +0900

    Update stratify, random sampler

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 dataname=$1
 n_class=18
 opt_list='Adam'
-loss_list='f1 CE'
+loss_list='f1 labelsmoothing' #focal f1
 lr_list='1e-3'
-aug_list='gray_crop' # random_all random_all1' #gaussian 
-bs_list='64'
-model_list='efficientnet_b0'
-p_list='0.5'
+aug_list='norm' # random_all random_all1' #gaussian 
+bs_list='32'
+model_list='efficientnet_b3'
+# p_list='0.5'
 
 
 for model in $model_list
@@ -19,33 +19,30 @@ do
             do
                 for aug in $aug_list
                 do
-                    for p in $p_list
+                
+                    for loss in $loss_list
                     do
-                        for loss in $loss_list
-                        do
-                            # use scheduler
-                            echo "model:$model, loss: $loss, bs: $bs, opt: $opt, lr: $lr, aug: $aug, p: $p use_sched: True"
-                            exp_name="${model}_${loss}_${lr}_${bs}_${aug}_scheduler_p=${p}_sampler"
-                            
-                            if [ -d "$exp_name" ]
-                            then
-                                echo "$exp_name is exist"
-                            else
-                                python ./CV-13/train.py \
-                                    --model $model \
-                                    --exp_name $exp_name \
-                                    --n_class $n_class \
-                                    --optimizer $opt \
-                                    --loss $loss\
-                                    --aug $aug \
-                                    --p $p \
-                                    --batch_size $bs \
-                                    --lr $lr \
-                                    --scheduler \
-                                    --epochs 50 \
-                                    --save_ckpt
-                            fi
-                        done
+                        # use scheduler
+                        echo "model:$model, loss: $loss, bs: $bs, opt: $opt, lr: $lr, aug: $aug, use_sched: True" # p: $p
+                        exp_name="${model}_${loss}_${lr}_${bs}_${aug}_scheduler_sampler_stratify"
+                        
+                        if [ -d "$exp_name" ]
+                        then
+                            echo "$exp_name is exist"
+                        else
+                            python ./CV-13/train.py \
+                                --model $model \
+                                --exp_name $exp_name \
+                                --n_class $n_class \
+                                --optimizer $opt \
+                                --loss $loss\
+                                --aug $aug \
+                                --batch_size $bs \
+                                --lr $lr \
+                                --scheduler \
+                                --epochs 50 \
+                                --save_ckpt
+                        fi
                     done
                     # # not use scheduler
                     # echo "loss: $loss, model:$model, bs: $bs, opt: $opt, lr: $lr, aug: $aug, use_sched: False"
